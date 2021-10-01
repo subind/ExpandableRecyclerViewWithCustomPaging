aae6b3950655a90b14f2ec69b84ce8c483f136bd
subind suresh <O21302@bankfab.com>
2021-10-01 19:23:32 +0530

    fix from previous commit

--- app/src/main/java/com/example/expandablerecyclerviewwithpaging/ui/news_intent/NewsIntent.kt
+++ app/src/main/java/com/example/expandablerecyclerviewwithpaging/ui/news_intent/NewsIntent.kt
@@ -3,7 +3,6 @@ package com.example.expandablerecyclerviewwithpaging.ui.news_intent
 sealed class NewsIntent {
 
     object FetchSources: NewsIntent()
-    //object FetchArticles: NewsIntent()
     data class FetchArticles(val sourceId: String): NewsIntent()
 
 }
